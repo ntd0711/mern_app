61c7bb07a048523a0cb03f1c9e4f06fb0f66b16f
NguyenDat <ntd07112000@gmail.com>
2022-02-02 21:43:54 +0700

    rebuild vercel

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import { NotFound, ScrollTop } from 'components';
 import RequireAuth from 'components/hocs/require-auth';
 import { AuthFeature, PostsFeature, ProfileFeature } from 'features';
+import EmptyLayout from 'layouts/empty-layout';
 import MainLayout from 'layouts/main-layout';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <ScrollTop />
       <Routes>
         <Route element={<MainLayout />}>
+          <Route path="/" element={<Navigate to="/posts" />} />
           <Route path="posts/*" element={<PostsFeature />} />
           <Route path="profile/*" element={<RequireAuth component={<ProfileFeature />} />} />
           <Route path="signin" element={<AuthFeature />} />
+        </Route>
+        <Route element={<EmptyLayout />}>
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
